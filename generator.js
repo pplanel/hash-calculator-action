c4c6af2376a231dae8f00d2503f6a6a10f475ebf
Pedro Gabriel Planel <pplanel@gmail.com>
2021-04-26 21:03:42 -0300

    Remove restriction

--- generator.js
+++ generator.js
@@ -3,10 +3,6 @@ const HashAlgorithmFactory = require('./lib/hash_algorithm_factory')
 let generator = function (hash_type, input_str, hash_output_length=undefined) {
     return new Promise((resolve) => {
 
-        if (hash_output_length !== undefined && hash_type !== 'SHA3' && hash_type !== 'SHA-3') {
-            throw new Error('Output length can only be used when using SHA3 method')
-        }
-
         const hash_algo = HashAlgorithmFactory
             .getHashAlgorithm(hash_type)
 
